0f810a011275379479becb07acad05f5809e981b
Martin Ankerl <Martin.Ankerl@gmail.com>
2016-09-09 06:18:31 +0200

    copyright header

--- examples/rastrigin.rs
+++ examples/rastrigin.rs
@@ -1,4 +1,11 @@
-// Simple example how to use the API.
+// Copyright 2016 Martin Ankerl. 
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 extern crate differential_evolution;
 
 use differential_evolution::self_adaptive_de;
